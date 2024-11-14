09fbdcc374a87308c480a63c00e489bb3acb2d29
Nick Moon <nick@nowplus.org>
2024-11-15 00:03:45 +0800

    Added GetPerson feature

--- backend/Alexandria.Api/Features/People/Endpoints/GetPerson.cs
+++ backend/Alexandria.Api/Features/People/Endpoints/GetPerson.cs
@@ -1,5 +1,7 @@
 using Alexandria.Api.Common.Interfaces;
 using Alexandria.Api.Domain;
+using Alexandria.Api.Features.People.DTOs;
+using Alexandria.Api.Infrastructure.Data;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,9 +15,24 @@ public class GetPerson : IEndpoint
         .WithName(nameof(GetPerson))
         .Produces<Ok<Person>>();
 
-    private static async Task<Results<Ok, NotFound>> Handle([FromRoute] Guid id)
+    private static async Task<Results<Ok<GetPersonResponse>, NotFound>> Handle(
+        [FromRoute] Guid id,
+        [FromServices] AppDbContext context)
     {
-        return TypedResults.Ok();
+        var person = await context.People.FindAsync(id);
+        if (person is null) return TypedResults.NotFound();
+
+        var response = new GetPersonResponse(new PersonDto
+        {
+            Id = person.Id,
+            FirstName = person.FirstName!,
+            LastName = person.LastName!,
+            MiddleNames = person.MiddleNames,
+            Description = person.Description,
+        });
+        
+        return TypedResults.Ok(response);
     }
 
+    private record GetPersonResponse(PersonDto Person);
 }
