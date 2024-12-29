b2a3e4adc5acf3c2820588c6218a3c04642749ac
Nick Moon <nick@nowplus.org>
2024-12-29 12:02:07 +0800

    Enabled CORS

--- backend/src/Alexandria.Api/ConfigureApp.cs
+++ backend/src/Alexandria.Api/ConfigureApp.cs
@@ -12,6 +12,7 @@ public static class ConfigureApp
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
+            app.UseCors(ConfigureServices.LocalHost5173CorsPolicy);
         }
 
         app.UseAuthentication();
