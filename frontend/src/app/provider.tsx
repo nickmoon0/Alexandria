ed1f6c3f1950dab0a3014e02f7c8dcb82c5dc678
Nick Moon <nick@nowplus.org>
2025-02-22 23:16:17 +0800

    Added toast component

--- frontend/src/app/provider.tsx
+++ frontend/src/app/provider.tsx
@@ -2,6 +2,7 @@ import { AuthProvider } from 'react-oidc-context';
 import { userManager, onSigninCallback } from '@/config/auth';
 import { ProtectedRoute } from '@/app/protectedRoute';
 import { EntriesRefreshProvider } from '@/features/entries/hooks/EntriesContext';
+import { ToastProvider } from '@/hooks/ToastContext';
 
 type AppProviderProps = {
   children: React.ReactNode;
@@ -11,9 +12,11 @@ export const AppProvider = ({ children }: AppProviderProps) => {
   return (
     <AuthProvider userManager={userManager} onSigninCallback={onSigninCallback}>
       <ProtectedRoute>
-        <EntriesRefreshProvider>
-          {children}
-        </EntriesRefreshProvider>
+        <ToastProvider>
+          <EntriesRefreshProvider>
+            {children}
+          </EntriesRefreshProvider>
+        </ToastProvider>
       </ProtectedRoute>
     </AuthProvider>
   );
---
+++ frontend/src/components/Toast.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import '@/components/styles/toast.css';
+
+interface ToastProps {
+  message: string;
+  type: ToastType;
+  onClose: () => void;
+};
+
+export enum ToastType {
+  Error,
+  Information
+};
+
+const Toast: React.FC<ToastProps> = ({ message, type, onClose }) => {
+  useEffect(() => {
+    const timer = setTimeout(onClose, 3000); // Auto-dismiss after 3s
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
+  const getToastStyling = () => {
+    switch(type) {
+      case ToastType.Error:
+        return 'toast-error';
+      case ToastType.Information:
+        return 'toast-information';
+    }
+  };
+
+  return <div className={getToastStyling()}>{message}</div>;
+};
+
+export default Toast;
